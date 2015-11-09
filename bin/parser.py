cd577da712c47c11af887bab1f2cb636bc6a6a46
Martin Storgaard <20140000@post.au.dk>
2015-11-09 14:33:42 +0100

    Added comments

--- bin/parser.py
+++ bin/parser.py
@@ -2,6 +2,10 @@ from Bio import Phylo
 from sys import argv
 
 if len(argv) != 2:
+	print 'Usage: python parser.py <newick-file>'
+	exit()
+elif not argv[1].endswith('.newick'):
+	print 'Only supports .newick files'
 	exit()
 
 tree = Phylo.read(argv[1], 'newick')
@@ -13,20 +17,29 @@ def to_adjacency_list(tree):
     lookup = {}
     adjlist = {}
     for i, elem in enumerate(allclades):
+		# Map app Clade-objects to a number
 		lookup[elem] = unicode(i)
+		# Represent the adjacency list as a set to avoid duplicates
 		adjlist[unicode(i)] = set()
     for parent in tree.find_clades(terminal=False, order='level'):
         for child in parent.clades:
+        	# We do not have any direction, hence parants are links to children and
+        	# visa versa
             adjlist[lookup[parent]].add(lookup[child])
             adjlist[lookup[child]].add(lookup[parent])
 
-    return (allclades, adjlist)
+    return adjlist
     
 
-clades, adjlist = to_adjacency_list(tree)
+adjlist = to_adjacency_list(tree)
 
-with open(argv[1][:-7] + ".adj", 'wb') as f:
+# Crop the ".newick" ending of the file and add ".adj"
+with open(argv[1][:-7] + '.adj', 'wb') as f:
+	# Write the number of nodes
 	f.write(unicode(len(adjlist)) + u"\n")
 	for k in sorted(adjlist.keys(), key=int):
-		adj = map(unicode, list(adjlist[k]))
+		adj = list(adjlist[k])
+		# Write the length of the adjecency list and then all adjacent nodes
 		f.write(unicode(len(adj)) + ' ' + ' '.join(adj) + "\n")
+		
+print 'Written to', argv[1][:-7] + '.adj'
